9da24d28f91aabd8ee197c509015b8b5976cc571
erladion <johan.jansson94@gmail.com>
2021-11-24 22:26:35 +0100

    Update to be able to be used on different distros, and add lsd

--- setup.fish
+++ setup.fish
@@ -1,8 +1,40 @@
 #!/usr/bin/fish
+if type -q dnf
+    set -gx packagemanager "dnf"
+    set -gx packagecommand "sudo $packagemanager install"
+else if type -q pacman
+    set -gx packagemanager "pacman"
+    set -gx packagecommand "sudo $packagemanager install"
+else if type -q apt
+    set -gx packagemanager "apt"
+    set -gx packagecommand "sudo $packagemanager install"
+else
+    #set -gx packagemanager "Unknown"
+end
+
+function installrust
+    curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+end
+
+function setuplsd
+    if ! type -q cargo
+        echo "Rust is not installed, installing it now"
+        installrust
+    end
+
+    if ! type -q lsd
+        echo "lsd is not installed, installing"
+        cargo install lsd
+    else
+        echo "lsd is already installed"
+    end
+    
+end
+
 function setuptmux
     if ! type -q tmux
         echo "Installing tmux"
-        sudo apt install tmux
+        $packagecommand tmux
     else
         echo "tmux is already installed"
     end
@@ -11,10 +43,15 @@ end
 function setupomf
     function installselected
         # TODO: Disgusting, fix this.
-        set choice (omf theme | string escape --style=regex | string replace -r '(.+Installed:)(.+)' '$2' | string replace -r '(.+Available:)(.+)' '$2' | string replace -a -r '[\\\]' '' | string replace -a -r "[[:blank:]]+" " " | string escape --style=regex | tr -d '[m' | tr -d '(B' | string split " " | string unescape | sort -u -r | fzf --preview-window=up:5% --preview="cat text.txt")
+        set choice (omf theme | string escape --style=regex | string replace -r '(.+Installed:)(.+)' '$2' | string replace -r '(.+Available:)(.+)' '$2' | string replace -a -r '[\\\]' '' | string replace -a -r "[[:blank:]]+" " " | string escape --style=regex | tr -d '[m' | tr -d '(B' | string split " " | string unescape | sort -u -r | fzf --preview-window=up:5% --preview="cat themechoice.txt")
 
         echo "Installing $choice"
         omf install $choice
+
+	if test -e ~/.config/fish/functions/fish_prompt.fish
+            rm ~/.config/fish/functions/fish_prompt.fish
+	end
+        omf reload
     end
 
     if ! type -q omf
@@ -24,7 +61,7 @@ function setupomf
             echo "Curl is not installed, aborting omf setup"
             return
         else
-            curl -L https://get.oh-my.fish > install    
+            curl -k -L https://get.oh-my.fish > install
             fish install --path=~/.local/share/omf --config=~/.config/omf
 
             installselected
@@ -40,7 +77,7 @@ function setupfzf
     function installfzf
         echo "Installing fzf"
         git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-        ~/.fzf/install
+        yes | ~/.fzf/install
     end
 
     if ! type -q fzf
@@ -62,9 +99,15 @@ else
     #cp settings.json $WSLPATH/AppData/Local/Packages/Microsoft.WindowsTerminal*/
 end
 
-setuptmux
-setupfzf
-setupomf
+if test -n "$packagemanager"
+    echo "Found package manager: $packagemanager"
+    setuptmux
+    setupfzf
+    setupomf
+    setuplsd
+else 
+    echo "Unknown package manager, aborting!"
+end
 
 # Setup fish config/functions
 #cp config.fish /home/$USER/.config/fish/
